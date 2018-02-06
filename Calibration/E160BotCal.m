4a22a44d5ceec9980a05058dd241b48b9ea00bcb
eyassushimelis <eyassushimelis@gmail.com>
2018-02-05 19:02:43 -0800

    Adding matlab scripts and code updates

---
+++ Calibration/E160BotCal.m
@@ -0,0 +1,118 @@
+% Eyassu Shimelis
+% E160 Lab 1: E160 Bot Distance Calibration
+% February 3, 2018
+
+load E160FrontCalData
+
+% True data collection points
+cmTrue = 15:5:100; cmTrue = cmTrue';
+
+cmMeasured = [mean(cm15), mean(cm20), mean(cm25), mean(cm30), ...
+          mean(cm35), mean(cm40), mean(cm45), mean(cm50), ...
+          mean(cm55), mean(cm60), mean(cm65), mean(cm70), ...
+          mean(cm75), mean(cm80), mean(cm85), mean(cm90), ...
+          mean(cm95), mean(cm100)]';
+
+% combine data vectors
+calData = [cmMeasured, cmTrue];
+
+% % calculate standard error
+% stdError = [std(cm15)/sqrt(length(cm15));
+%             std(cm20)/sqrt(length(cm20));
+%             std(cm25)/sqrt(length(cm25));
+%             std(cm30)/sqrt(length(cm30));
+%             std(cm35)/sqrt(length(cm35));
+%             std(cm40)/sqrt(length(cm40));
+%             std(cm45)/sqrt(length(cm45));
+%             std(cm50)/sqrt(length(cm50));
+%             std(cm55)/sqrt(length(cm55));
+%             std(cm60)/sqrt(length(cm60));
+%             std(cm65)/sqrt(length(cm65));
+%             std(cm70)/sqrt(length(cm70));
+%             std(cm75)/sqrt(length(cm75));
+%             std(cm80)/sqrt(length(cm80));
+%             std(cm85)/sqrt(length(cm85));
+%             std(cm90)/sqrt(length(cm90));
+%             std(cm95)/sqrt(length(cm95));
+%             std(cm100)/sqrt(length(cm100))];
+         
+stdError = [std(cm15);
+    std(cm20);
+    std(cm25);
+    std(cm30);
+    std(cm35);
+    std(cm40);
+    std(cm45);
+    std(cm50);
+    std(cm55);
+    std(cm60);
+    std(cm65);
+    std(cm70);
+    std(cm75);
+    std(cm80);
+    std(cm85);
+    std(cm90);
+    std(cm95);
+    std(cm100)];
+        
+% remove 15cm measurement
+cmMeasured = cmMeasured(2:end);
+cmTrue = cmTrue(2:end); 
+stdError = stdError(2:end);
+
+logFit = fittype('a + b*log(x)',...
+'dependent',{'y'},'independent',{'x'},...
+'coefficients',{'a','b'});
+
+inverseFit = @(c, x) c(1) + c(2)./(x.^c(3));
+inverseFitCoeff = nlinfit(cmMeasured, cmTrue, inverseFit, [1, 1, 0.5]);
+
+
+logFit = fit(cmMeasured, cmTrue, logFit);
+[polyFit, polyErr] = polyfit(cmMeasured, cmTrue, 2);
+
+% calculate residuals
+polyResid = cmTrue - polyval(polyFit, cmMeasured);
+polySSresid = sum(polyResid.^2);
+polySStotal = (length(cmTrue) - 1) * var(cmTrue);
+polyrsq = 1 - polySSresid/polySStotal;
+
+logResid = cmTrue - logFit(cmMeasured);
+logSSresid = sum(logResid.^2);
+logSStotal = (length(cmTrue) - 1) * var(cmTrue);
+logrsq = 1 - logSSresid/logSStotal;
+
+inverseResid = cmTrue - inverseFit(inverseFitCoeff, cmMeasured);
+inverseSSresid = sum(inverseResid.^2);
+inverseSStotal = (length(cmTrue) - 1) * var(cmTrue);
+inversersq = 1 - inverseSSresid/inverseSStotal;
+
+% plotting
+figure(1); clf; hold on;
+set(groot,'defaulttextinterpreter','latex');  
+set(groot, 'defaultAxesTickLabelInterpreter','latex');  
+set(groot, 'defaultLegendInterpreter','latex');
+
+% actual results
+plot(cmMeasured, cmTrue, 'blx');
+
+% plot fits
+plot(cmMeasured, logFit(cmMeasured), 'Color', [0,0.4470,0.7410], 'LineWidth', 2, 'LineStyle', '--');
+plot(cmMeasured, polyval(polyFit, cmMeasured), 'Color', [0.8500,0.3250,0.0980], 'LineWidth', 2, 'LineStyle', ':');
+plot(cmMeasured, inverseFit(inverseFitCoeff, cmMeasured), 'Color', [0.4940,0.1840,0.5560], 'LineWidth', 2, 'LineStyle', '-');
+
+legend('Measured Digital Voltage', ...
+    ['Log Fit, R$^2$ = ', num2str(logrsq)], ...
+    ['2nd Order Polynomial Fit, R$^2$ = ', num2str(polyrsq)], ...
+    ['Nonlinear Inverse Fit, R$^2$ = ', num2str(inversersq)])
+
+title('Sharp Distance Sensor Calibration Fitting');
+ylabel('Actual Distance (cm)');
+xlabel('Measured Distance (ADC Output)');
+set(gca, 'fontsize', 28);
+grid on; grid minor;
+
+% plot error
+errorbar(cmMeasured, cmTrue, ones(length(cmTrue), 1).*0.3, 'LineStyle', 'none')
+herrorbar(cmMeasured, cmTrue, stdError);
+hold off;
--- Code/E160_robot.py
+++ Code/E160_robot.py
@@ -17,78 +17,118 @@ class E160_robot:
         self.wheel_radius = 0.03
         self.address = address
         self.ID = self.address.encode().__str__()[-1]
+
         self.last_measurements = []
+        self.MAX_PAST_MEASUREMENTS = 10
+
         self.robot_id = robot_id
         self.manual_control_left_motor = 0
         self.manual_control_right_motor = 0
         self.file_name = 'Log/Bot' + str(self.robot_id) + '_' + datetime.datetime.now().replace(microsecond=0).__str__() + '.txt'
         self.make_headers()
 
+        self.front_dist = 0
+        self.left_dist = 0
+        self.right_dist = 0
+
+        self.control_effort = 0
+        self.error = 0
+
     def update(self, deltaT):
-        
+
         # get sensor measurements
         encoder_measurements, range_measurements = self.update_sensor_measurements()
 
+        # update robot state
+        [self.front_dist, self.right_dist, self.left_dist] = range_measurements
+
         # localize
         self.localize(encoder_measurements, range_measurements, deltaT)
-        
+
         # call motion planner
         #self.motion_planner.update_plan()
-        
+
         # determine new control signals
         R, L = self.update_control(range_measurements)
-        
+
         # send the control measurements to the robot
         self.send_control(R, L, deltaT)
-    
-    
+
+
     def update_sensor_measurements(self):
-        
+
         # send to actual robot !!!!!!!!! John
         if self.environment.robot_mode == "HARDWARE MODE":
             command = '$S @'
             self.environment.xbee.tx(dest_addr = self.address, data = command)
-            
+
             update = self.environment.xbee.wait_read_frame()
-            
+
             data = update['rf_data'].decode().split(' ')[:-1]
             data = [int(x) for x in data]
             encoder_measurements = data[-2:]
             range_measurements = data[:-2]
-            
-            print "update sensors measurements ",encoder_measurements, range_measurements
+
+            # solution is of the form y = c1 + c2/(x^c3), where the coefficients were
+            # calculated in MATLAB
+            c = [-17.5818421751112, 7402.73728818712, 0.791041234703247]
+
+            # convert range_measurements to cm using non linear inverse fit
+            for i in range(len(range_measurements)):
+                
+                x = range_measurements[i]
+
+                range_measurements[i] = c[0] + c[1]/(x**c[2])
+
+            # keep track of measurement
+            if len(self.last_measurements) < self.MAX_PAST_MEASUREMENTS:
+            	self.last_measurements.append(range_measurements)
+
+            else: 
+            	self.last_measurements.pop(0)
+            	self.last_measurements.append(range_measurements)
+
+            # update current measurement to robot state
+            self.currentRangeMeasurement = range_measurements
+
             return encoder_measurements, range_measurements
 
-        
+
         # obtain sensor measurements !!!!!! Chris
         elif self.environment.robot_mode == "SIMULATION MODE":
             return 1,1
-        
+
         return 1,1
-        
-        
+
+
     def localize(self, encoder_measurements, range_measurements, deltaT):
         pass
-    
-    
+
+
     def update_control(self, range_measurements):
-        
+
         if self.environment.control_mode == "MANUAL CONTROL MODE":
             R = self.manual_control_right_motor
             L = self.manual_control_left_motor
+
+        elif self.environment.control_mode == "AUTONOMOUS CONTROL MODE":
+
+            KPGain = 2
+            KIGain = .1
+            desiredDistance = 30
+
+            integralError = [x[0] - desiredDistance for x in self.last_measurements]
             
-        elif self.environment.control_mode == "AUTONOMOUS CONTROL MODE":        
-            
-            # Put student code for lab 1 control here!!!!!!
-            # Don't keep the R=0, L=0 code
-            R=0
-            L=0
-            
-        
+            self.error = range_measurements[0] - desiredDistance
+            self.control_effort = KPGain * self.error + KIGain*sum(integralError)
+
+            R = self.control_effort
+            L = self.control_effort
+
         return R, L
-            
+
     def send_control(self, R, L, deltaT):
-        
+
         # send to actual robot !!!!!!!!
         if self.environment.robot_mode == "HARDWARE MODE":
             if (L < 0):
@@ -105,46 +145,42 @@ class E160_robot:
 
             command = '$M ' + str(LDIR) + ' ' + str(LPWM) + ' ' + str(RDIR) + ' ' + str(RPWM) + '@'
             self.environment.xbee.tx(dest_addr = self.address, data = command)
-            
-        
+
+
         # simulate kinematics
         elif self.environment.robot_mode == "SIMULATION MODE":
             self.state = self.update_state(deltaT, self.state, R, L)
 
-        
+
     def update_state(self, deltaT, state, R, L):
-        
+
 
         return state
-        
-        
-        
+
+
+
     def make_headers(self):
         f = open(self.file_name, 'a+')
-        f.write('{0} {1:^1} {2:^1} {3:^1} {4:^1} \n'.format('R1', 'R2', 'R3', 'RW', 'LW'))
+        f.write('{0} {1:^1} {2:^1} {3:^1} {4:^1} {5:^1} {6:^1} \n'.format('F,', 'L,', 'R,', 'RW,', 'LW,', 'CE,', 'E'))
         f.close()
 
-        
-        
+
+
     def log_data(self):
         f = open(self.file_name, 'a+')
-        
+
         # edit this line to have data logging of the data you care about
-        data = [str(x) for x in [1,2,3,4,5]]
-        
+        data = [str(x) + "," for x in [self.front_dist, self.left_dist, self.right_dist, self.manual_control_right_motor, self.manual_control_left_motor, self.control_effort, self.error]]
+
         f.write(' '.join(data) + '\n')
         f.close()
-        
-        
+
+
     def set_manual_control_motors(self, R, L):
-        
+
         if self.environment.robot_mode == "HARDWARE MODE":
             self.manual_control_right_motor = int(R*256/100)
-            self.manual_control_left_motor = int(L*256/100)           
+            self.manual_control_left_motor = int(L*256/100)
         else:
             self.manual_control_right_motor = float(R)/500
             self.manual_control_left_motor = float(L)/500
-                                              
-   
-
-
