146f038c7c93684fefa38e56b4aa885e92151fa7
eyassushimelis <eyassushimelis@gmail.com>
2018-02-24 19:25:06 -0800

    Finished point tracking

--- Code/util.py
+++ Code/util.py
@@ -1,21 +1,9 @@
 import math
 
-# def angDiff(theta1, theta2):
-
-
-
-
-# bound angles in 
-def boundAngleRad(theta):    
-    while(theta>math.pi):
-        theta -= 2.0*math.pi
-    while(theta<math.pi):
-        theta += 2.0*math.pi
-    return theta
-
-def boundAngleDeg(theta):
-    while(theta>180):
-        theta -= 360
-    while(theta<180):
-        theta += 360
-    return theta
+def angle_wrap(a):
+    while a > math.pi:
+        a = a - 2*math.pi
+    while a < -math.pi:
+        a = a + 2*math.pi
+        
+    return a
